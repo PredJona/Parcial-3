0ecff0c6f52037a93a5f46050aa4f4449af39756
josemartinez49 <jose.martinez49@utp.ac.pa>
2026-07-01 08:55:23 -0500

    cambios interfaz2

--- P3_LPI_Eq03_RegEst/obj/Debug/net48/P3_LPI_Eq03_RegEst.AssemblyInfo.cs
+++ P3_LPI_Eq03_RegEst/obj/Debug/net48/P3_LPI_Eq03_RegEst.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P3_LPI_Eq03_RegEst")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b170cd81b7284df1ac1717780b3622f29268e51e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d34a994d8ee326c7bea0d8d3b23d66891fbfc6d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("P3_LPI_Eq03_RegEst")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P3_LPI_Eq03_RegEst")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
